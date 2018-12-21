62f4531962f6d452b46ab2baf6dde13cf4c146bc
Clara <cphilippzig@googlemail.com>
2018-12-21 13:18:12 +0100

    add sound test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 /* eslint-disable */
+AFRAME.registerComponent('soundhandler', {
+    tick: function () {
+           var entity = document.querySelector('[sound]');
+         if (document.querySelector('a-marker').object3D.visible == true) {
+            entity.components.sound.playSound();
+        } else {
+            entity.components.sound.pauseSound();
+        }
+
+     }
+});
